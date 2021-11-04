e073f9555b244da062518e07f1f22aa2bb951c41
Adolfo Roman <72475943+adolfo-roman@users.noreply.github.com>
2021-11-04 00:16:19 -0600

    Delete practica2 directory

--- practica2/codigo/ejercicio1/ordenamientos.h
+++
@@ -1,8 +0,0 @@
-void selectionSort(int arreglo[], int n);
-void insertionSort(int a[], int n);
-void HeapSort(int a[], int n);
-void BuildHeap(int* A, int size);
-void Heapify(int* A, int i, int size);
-int partition (int arr[], int low, int high);
-void quickSort(int arr[], int low, int high);
-void bubbleSort(int a[], int size);
